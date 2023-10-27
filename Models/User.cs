dde7eee9b71a0b6e6a76b2659b8f8256071f12d1
Mahmoud-Elsamman <mahmoud.elsamman97@gmail.com>
2023-10-28 01:40:17 +0200

    Create User model

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_core_API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public List<Character> Characters { get; set; }
+    }
+}

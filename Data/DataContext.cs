8b275d135b9ae20c5b37541bd142d3a19a87e916
Mahmoud-Elsamman <mahmoud.elsamman97@gmail.com>
2023-10-28 01:55:19 +0200

    Add CharacterSkill composite key

---
+++ Data/DataContext.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_core_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_core_API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
+
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<CharacterSkill> CharacterSkills { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CharacterSkill>()
+                .HasKey(cs => new { cs.CharacterId, cs.SkillId });
+        }
+
+    }
+}

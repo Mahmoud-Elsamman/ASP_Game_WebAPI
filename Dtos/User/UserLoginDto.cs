ca4bd8f40b3a36f250f0af55d1193e88b06362e3
Mahmoud-Elsamman <mahmoud.elsamman97@gmail.com>
2023-10-28 01:45:03 +0200

    Create User DTOs

---
+++ Dtos/User/UserLoginDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_core_API.Dtos.User
+{
+    public class UserLoginDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}

1dda99ac14146545282f88139582f22e238f907e
Mahmoud-Elsamman <mahmoud.elsamman97@gmail.com>
2023-10-28 02:01:25 +0200

    Add Character Skill relation Migration

---
+++ Migrations/20230922174930_UserCharacteRelation.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASP_core_API.Migrations
+{
+    public partial class UserCharacteRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Characters",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Characters_UserId",
+                table: "Characters",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Users_UserId",
+                table: "Characters",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Users_UserId",
+                table: "Characters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Characters_UserId",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Characters");
+        }
+    }
+}

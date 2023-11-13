0dc0b332e2309005be78619a4c6ed005aec59eab
Mahmoud-Elsamman <mahmoud.elsamman97@gmail.com>
2023-11-13 16:31:37 +0200

    add the role to the user

--- Dtos/User/UserRegisterDto.cs
+++ Dtos/User/UserRegisterDto.cs
@@ -9,5 +9,6 @@ namespace ASP_core_API.Dtos.User
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Role { get; set; }
     }
 }

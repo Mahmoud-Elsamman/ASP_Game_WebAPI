31d4b28548e4f75b63c76506254ac2b180a34d0a
Mahmoud-Elsamman <mahmoud.elsamman97@gmail.com>
2023-11-13 16:30:42 +0200

    create the fight endpoint

---
+++ Controllers/FightController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_core_API.Dtos.Fight;
+using ASP_core_API.Services.FightService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_core_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FightController : ControllerBase
+    {
+        private readonly IFightService _fightService;
+        public FightController(IFightService fightService)
+        {
+            _fightService = fightService;
+
+        }
+
+        [HttpPost("Weapon")]
+        public async Task<IActionResult> WeaponAttack(WeaponAttackDto request)
+        {
+            return Ok(await _fightService.WeaponAttack(request));
+        }
+
+        [HttpPost("Skill")]
+        public async Task<IActionResult> SkillAttack(SkillAttackDto request)
+        {
+            return Ok(await _fightService.SkillAttack(request));
+        }
+    }
+}
